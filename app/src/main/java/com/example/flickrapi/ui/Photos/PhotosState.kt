6c8b48f18a5053b5b85b4deeeb3543d5e18d571f
Alselwi <alselwi@Alselwis-MacBook-Pro.local>
2023-07-05 18:45:49 +0100

    Changes added

--- app/src/main/java/com/example/flickrapi/ui/Photos/PhotosState.kt
+++ app/src/main/java/com/example/flickrapi/ui/Photos/PhotosState.kt
@@ -1,9 +1,10 @@
 package com.example.flickrapi.ui.Photos
 
+import com.example.flickrapi.data.allPhotos.GetPhotos
 import com.example.flickrapi.data.allPhotos.Photo
 
 data class PhotosState(
     val isLoading: Boolean = false,
-    var photos: List<Photo>? = null,
+    var photos: GetPhotos? = null,
     val error: String = ""
 )
